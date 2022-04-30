4b196253223026266c08cf2be924243733ed886a
storres20 <sanchotorres777@gmail.com>
2022-04-29 21:55:45 -0500

    ADD scraper

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -3,8 +3,20 @@ import puppeteer from "puppeteer";
 (async () => {
 
   const browser = await puppeteer.launch({ headless: false });
-      const page = await browser.newPage();
-      await page.goto('https://www.sofascore.com/');
+ 
+    
+    const page = await browser.newPage();
+    await page.setViewport( {width: 1000, height: 1080});
+
+      await page.setDefaultNavigationTimeout(0);
+      await page.goto('https://www.sofascore.com/', {
+
+      waitUntil: "load",
+      timeout: 0
+      });
+
+      
+
       await page.waitForTimeout(3000)
   await browser.close();
 })();
